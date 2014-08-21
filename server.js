3560a84e53c99305a654f743c3b60ac66a26d15d
Ray Farias <ray@rayfarias.com>
2014-08-20 15:41:46 -1000

    add index controller to server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+var jade = require('jade');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+var index = require('./routes/index');
+
+var server = express();
+
+// view engine setup
+server.set('views', path.join(__dirname, 'views'));
+server.set('view engine', 'jade');
+
+// method definitions
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// set public directory
+server.use(express.static(path.join(__dirname, 'public')));
+
+server.use('/', index);
+
+server.listen(3000, console.log("pizza server enabled!"));
