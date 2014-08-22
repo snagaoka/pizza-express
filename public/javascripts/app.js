a5a20577313af94a5161efb05e1653d2a37e1219
snagaoka <shannonnagaoka@gmail.com>
2014-08-21 19:42:04 -1000

    appended each order to order queue, displays in client side

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,19 +1,39 @@
 $(function(){
-	$.ajax("/orders", {
-		method: "GET",
-		success: function(data){
-			for (var i = 0; i < data.length; i++) {
-				console.log(data[i].size);
-				data[i].toppings.meats.forEach(function(element, index){
-					console.log(element);
-				});
-				data[i].toppings.veggies.forEach(function(element, index){
-					console.log(element);
-				});
-			}
-		}
-		// failure: function(error){
 
+	$.ajax("/orders", {
+
+		method: "GET",
+
+		success: function(data){
+
+			var orderQueue = $("<ul></ul>");
+			
+			for (var i = 0; i < data.length; i++) {
+
+				var eachOrder = $("<li></li>");
+
+					eachOrder.append(data[i].size); // show size in order queue
+
+					data[i].toppings.meats.forEach(function(element, index){
+						eachOrder.append(element); // show meats in order queue
+					});
+
+					data[i].toppings.veggies.forEach(function(element, index){
+						eachOrder.append(element); // show meats in order queue
+					});
+
+				orderQueue.append(eachOrder);
+
+			}
+
+			$(".order-list").append(orderQueue);
+
+		}
+
+		// failure: function(error){
+			// if(err)res.send(500, err);
+			// console.log('error');
 		// }
 	});
+	
 });
