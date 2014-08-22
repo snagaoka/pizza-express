a471a41073b5fdc34dfd19c24654957303ba5ee9
snagaoka <shannonnagaoka@gmail.com>
2014-08-21 21:48:04 -1000

    added ajax POST request to submit order

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,32 +1,43 @@
 $(function(){
 
+	$("#form").submit(function(event){
+		event.preventDefault();
+		$.ajax("/orders", {
+			method: "POST",
+			data: $('#form').serialize(),
+			success: function(data){ // put order (object) into an array
+				var currentOrder = [data]; 
+				showOrderQueue(currentOrder); // calling showOrderQueue with currentOrder
+			},
+			failure: function(error){
+				console.log('error')
+			}
+		});
+	});
+
 	$.ajax("/orders", {
-
 		method: "GET",
+		success: showOrderQueue,
+		failure: function(error){
+			console.log('error');
+		}	
+	});
 
-		success: function(data){
+	function showOrderQueue(data){ 
 
-			var orderQueue = $("<ul></ul>");
-			
+			var orderQueue = $("ul.order-list");
+
 			for (var i = 0; i < data.length; i++) {
-
+				console.log(i);
 				var eachPizzaSize = data[i].size;
 				var eachPizzaMeatTopping = data[i].toppings.meats;
 				var eachPizzaVeggieTopping = data[i].toppings.veggies;
 				var eachPizza = eachPizzaSize + eachPizzaMeatTopping + eachPizzaVeggieTopping
-
 				orderQueue.append("<li>" + eachPizza + "</li>");
-
 			}
-
-			$(".order-list").append(orderQueue);
-
+			// $(".order-list").append();
 		}
 
-		// failure: function(error){
-			// if(err)res.send(500, err);
-			// console.log('error');
-		// }
-	});
-	
 });
+
+// static items (form, work-header) go in HTML, dynamic items (order queue) in JS
